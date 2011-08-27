8f6a380d0cabde7a3d5f330f66ded6894fd46c05
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2011-08-27 11:50:26 +0530

    chpt1 ex1.3

---
+++ src/sicp/chpt1/ex1_3.clj
@@ -0,0 +1,30 @@
+(ns sicp.chpt1.ex1-3)
+
+
+(defn sqr
+  [x]
+  (* x x))
+
+
+(defn sum-of-squares
+  [x y]
+  (+ (sqr x) (+ sqr y)))
+
+
+(defn sum-of-greater-squares
+  [x y z]
+  (cond                                
+   (and (< x y) (< x z))                ; x < y,z 
+   (sum-of-squares y z)
+
+   (< x y)                              ; z < x < y
+   (sum-of-squares x y)
+   
+   (< x z)                              ; y < x < z
+   (sum-of-squares x z)                 
+
+   (< y z)                              ; y < z < x
+   (sum-of-squares z x)
+
+   :else                                ; z < y < x
+   (sum-of-squares y x)))
