752ad0cb9ffb680f8db66e9dad29b8d82053e806
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2011-08-27 11:07:53 +0530

    dev dependency for swank

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject sicp "0.1.0"
   :description "SICP in Clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
