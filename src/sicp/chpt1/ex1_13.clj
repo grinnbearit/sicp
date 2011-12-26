ec313ea45553a32b9ca392a7b4d362157101fe89
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2011-12-26 22:14:32 +0530

    chpt1 ex1.13

---
+++ src/sicp/chpt1/ex1_13.clj
@@ -0,0 +1,45 @@
+(ns sicp.chpt1.ex1-13)
+
+
+;;; fib(n) = fib(n-1) + fib(n-2), fib(1) = 1, fib(2) = 2
+
+;;; To prove:
+;;; fib(n) = (phi^n - psi^n)/sqrt(5)
+;;; where phi = (1 + sqrt(5))/2
+;;;       psi = (1 - sqrt(5))/2
+
+;;; Testing:
+;;; fib(1) = 1, true
+;;; fib(2) = 2, true
+
+;;; Assume fib(k-1), fib(k) is true
+
+;;; To prove fib(k+1) = fib(k) + fib(k-1)
+;;; fib(k-1) = (phi^(k-1) - psi^(k-1))/sqrt(5) + (phi^k - psi^k)/sqrt(5)
+
+;;; this can be rearranged as
+;;; (phi^k(1-phi) - psi^k(1-psi))/2
+
+;;; fib(k+1) = fib(k) + fib(k-1) when phi/psi take the value of x in
+;;; x^k(1-x) = x^(k+1)
+
+;;; Solving for x
+;;; x^2 - x - 1 = 0
+
+;;; Roots
+;;; (1 + sqrt(5))/2, (1 - sqrt(5))/2
+
+;;; Possible combinations of phi and psi
+
+;;; phi = (1 - sqrt(5))/2, psi = (1 - sqrt(5))/2
+;;; Invalid, fib(n) = 0 for all n > 0
+
+;;; phi = (1 + sqrt(5))/2, psi = (1 + sqrt(5))/2
+;;; Invalid, fib(n) = 0 for all n > 0
+
+;;; phi = (1 - sqrt(5))/2, psi = (1 + sqrt(5))/2
+;;; Invalid, fib(n) < 0 for all n > 0
+
+
+;;; phi = (1 + sqrt(5))/2, psi = (1 - sqrt(5))/2
+;;; Valid, therefore proved by induction
