fca78982af95989de7204a47b4a620de395846be
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-01-03 12:24:28 +0530

    chpt1 ex1.18

---
+++ src/sicp/chpt1/ex1_18.clj
@@ -0,0 +1,26 @@
+(ns sicp.chpt1.ex1-18
+  (:refer-clojure :exclude [double]))
+
+
+;;; For readability, double and halve
+
+(def double #(bit-shift-left % 1))
+(def halve #(bit-shift-right % 1))
+
+
+;;; Iterative version of fast-*
+;;; Keeping b*a + n constant in every iteration
+
+
+(defn fast-*
+  ([a b]
+     (fast-* a b 0))
+  ([a b n]
+     (cond (zero? b)
+           n
+
+           (even? b)
+           (recur (double a) (halve b) n)
+
+           :else
+           (recur a (dec b) (+ a n)))))
