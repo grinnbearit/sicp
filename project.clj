284a236aaa95c3a80fa48ae8368bf1f0704e4914
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-01-06 12:40:36 +0530

    SICP now uses clojure 1.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject sicp "0.1.0"
   :description "SICP in Clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
