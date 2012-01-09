1f23f407aab15a1d551c38d8dcc9de53937e9edc
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-01-09 16:54:19 +0530

    chpt1 ex1.21

---
+++ src/sicp/chpt1/ex1_21.clj
@@ -0,0 +1,31 @@
+(ns sicp.chpt1.ex1-21)
+
+
+(defn divides?
+  [a b]
+  (zero? (rem a b)))
+
+
+;;; Using multi arity functions instead
+(defn find-smallest-divisor
+  ([n]
+     (find-smallest-divisor n 2))
+  ([n test-divisor]
+     (cond (> (* test-divisor test-divisor) n)
+           n
+
+           (divides? n test-divisor)
+           test-divisor
+
+           :else
+           (recur n (inc test-divisor)))))
+
+
+;;; (find-smallest-divisor 199)
+;;; => 199
+
+;;; (find-smallest-divisor 1999)
+;;; => 1999
+
+;;; (find-smallest-divisor 19999)
+;;; => 7
