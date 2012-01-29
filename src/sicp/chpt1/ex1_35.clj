0ddb50492eea4540d317396f0378b292fbbdad74
Sidhant Godiwala <sidhant@glados.glados>
2012-01-29 13:10:09 +0530

    chpt1 ex1.35

---
+++ src/sicp/chpt1/ex1_35.clj
@@ -0,0 +1,34 @@
+(ns sicp.chpt1.ex1-35)
+
+
+;; x -> 1 + 1/x
+;; is a transformation whose fixed point is the solution to the equation
+;; x^2 = x + 1 which is also the definition for the golden ratio, phi
+
+
+(defn abs
+  "Because Math/abs does not work for rationals"
+  [x]
+  (if (< x 0)
+    (- x)
+    x))
+
+
+(def tolerance 1/100000)
+
+(defn fixed-point
+  [f first-guess]
+  (letfn [(close-enough? [v1 v2]
+            (< (abs (- v1 v2)) tolerance))
+
+          (try-it [guess]
+            (let [next (f guess)]
+              (if (close-enough? guess next)
+                next
+                (recur next))))]
+
+    (try-it first-guess)))
+
+
+(def phi
+  (fixed-point (fn [x] (+ 1 (/ x))) 1))
