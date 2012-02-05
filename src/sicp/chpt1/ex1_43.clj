5bec4549a05603bd1a7b9fe43a0172e80849cf17
Sidhant Godiwala <sidhant@glados.glados>
2012-02-05 17:24:08 +0530

    chpt1 ex1.43

---
+++ src/sicp/chpt1/ex1_43.clj
@@ -0,0 +1,17 @@
+(ns sicp.chpt1.ex1-43)
+
+
+(defn repeated
+  [f n]
+  (loop [g f i n]
+    (cond (= 1 i)
+          g
+
+          (even? i)
+          (recur (comp g g) (/ i 2))
+
+          :else
+          (recur (comp f g) (dec i)))))
+
+
+;; ((repeated #(* % %) 2) 5) => 625
