95e7c7dc746478b741a65aad376934bfbdac9a9f
Sidhant Godiwala <sidhant@glados.glados>
2012-02-18 09:57:13 +0530

    chpt2 ex2.1

---
+++ src/sicp/chpt2/ex2_1.clj
@@ -0,0 +1,22 @@
+(ns sicp.chpt2.ex2-1)
+
+
+;;; Using Clojure's records instead of a cons cell
+(defrecord Rational [n d])
+
+
+(defn- gcd
+  [x y]
+  (if (zero? y)
+    x
+    (recur y (rem x y))))
+
+
+(defn make-rat
+  [n d]
+  (let [div (Math/abs (gcd n d))
+        n-div (if (or (and (pos? n) (neg? d))
+                      (and (neg? n) (neg? d)))
+                    (- div)
+                    div)]
+    (Rational. (/ n n-div) (/ d n-div))))
