c2ddc18706538d621d17f848019a2ff84f1a4ca8
Sidhant Godiwala <sidhant@glados.glados>
2012-02-28 19:27:46 +0530

    chpt1 ex2.4

---
+++ src/sicp/chpt2/ex2_4.clj
@@ -0,0 +1,24 @@
+(ns sicp.chpt2.ex2-4)
+
+
+(defn cons
+  [x y]
+  (fn [m] (m x y)))
+
+
+(defn car
+  [z]
+  (z (fn [x y] x)))
+
+
+(defn cdr
+  [z]
+  (z (fn [x y] y)))
+
+
+;;; (cons 2 3)
+;;; (car (cons 2 3))
+;;; => 2
+
+;;; (cdr (cons 2 3))
+;;; => 3
