669d25ed1898ce984e1fef0b66a69302b7b72b40
Sidhant Godiwala <sidhant@glados.glados>
2012-02-28 19:40:04 +0530

    chpt1 ex2.5

---
+++ src/sicp/chpt2/ex2_5.clj
@@ -0,0 +1,33 @@
+(ns sicp.chpt2.ex2-5)
+
+
+(defn cons
+  [x y]
+  (int (* (Math/pow 2 x)
+          (Math/pow 3 y))))
+
+
+(defn- d-component
+  [n d]
+  (loop [x n acc 0]
+    (if (zero? (rem x d))
+      (recur (/ x d) (inc acc))
+      acc)))
+
+
+(defn car
+  [z]
+  (d-component z 2))
+
+
+(defn cdr
+  [z]
+  (d-component z 3))
+
+
+;;; (cons 2 3)
+;;; (car (cons 2 3))
+;;; => 2
+
+;;; (cdr (cons 2 3))
+;;; => 3
