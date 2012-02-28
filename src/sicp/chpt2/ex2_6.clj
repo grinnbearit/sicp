bb1292ad7f1ca3d608ca5def564ba060b19181d2
Sidhant Godiwala <sidhant@glados.glados>
2012-02-28 21:29:51 +0530

    chpt1 ex2.6

---
+++ src/sicp/chpt2/ex2_6.clj
@@ -0,0 +1,51 @@
+(ns sicp.chpt2.ex2-6)
+
+
+(defn church->int
+  [n]
+  ((n inc) 0))
+
+
+(def zero
+  (fn [f]
+    (fn [x]
+      x)))
+
+
+(defn add1
+  [n]
+  (fn [f]
+    (fn [x]
+      (f ((n f) x)))))
+
+
+(def one
+  (fn [f]
+    (fn [x]
+      (f x))))
+
+
+(def two
+  (fn [f]
+    (fn [x]
+      (f (f x)))))
+
+
+(defn add
+  [a b]
+  (fn [f]
+    (fn [x]
+      ((comp (a f) (b f)) x))))
+
+
+;;; (church->int zero)
+;;; => 0
+
+;;; (church->int one)
+;;; => 1
+
+;;; (church->int two)
+;;; => 2
+
+;;; (church->int (add one two))
+;;; => 3
