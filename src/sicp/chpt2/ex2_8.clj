75e75a584187855be554b66acb3a450608465665
Sidhant Godiwala <sidhant@glados.glados>
2012-03-05 19:28:22 +0530

    chpt2 ex2.8

---
+++ src/sicp/chpt2/ex2_8.clj
@@ -0,0 +1,30 @@
+(ns sicp.chpt2.ex2-8)
+
+
+(defrecord Interval [lb ub])
+
+
+(defn make-interval
+  [lb ub]
+  (Interval. lb ub))
+
+
+(defn lower-bound
+  [i]
+  (.lb i))
+
+
+(defn upper-bound
+  [i]
+  (.ub i))
+
+
+(defn sub-interval
+  [x y]
+  (let [a (- (lower-bound x)
+             (lower-bound y))
+        b (- (upper-bound x)
+             (upper-bound y))]
+    (if (< a b)
+      (make-interval a b)
+      (make-interval b a))))
