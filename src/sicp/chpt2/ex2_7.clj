be324bec21873d73c23c7bcd3f6ddcdfbcaae33b
Sidhant Godiwala <sidhant@glados.glados>
2012-03-05 19:23:52 +0530

    chpt2 ex2.7

---
+++ src/sicp/chpt2/ex2_7.clj
@@ -0,0 +1,19 @@
+(ns sicp.chpt2.ex2-7)
+
+
+(defrecord Interval [lb ub])
+
+
+(defn make-interval
+  [lb ub]
+  (Interval. lb ub))
+
+
+(defn lower-bound
+  [i]
+  (.lb i))
+
+
+(defn upper-bound
+  [i]
+  (.ub i))
