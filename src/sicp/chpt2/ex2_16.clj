2b8beda87a35a930ee39d5013ca30bdeee0f6aa8
Sidhant Godiwala <sidhantgodiwala@morrigan.local>
2012-04-20 09:04:32 +0530

    chpt2 ex2.16

---
+++ src/sicp/chpt2/ex2_16.clj
@@ -0,0 +1,15 @@
+(ns sicp.chpt2.ex2-16)
+
+
+;;; Regular algebra assumes that identities always have a fixed value
+;;; that's why identical identites can cancel each other out perfectly
+
+;;; With intervals that's not the case. The value is not precise and so normal
+;;; algebraic simplifications can't work.
+
+;;; I don't think it is possible to design an interval-arithmetic library
+;;; that does not have this problem.
+
+;;; What we can do is to express our equations with no simplifications using standard
+;;; algebraic assumptions (like N/N, n!=0 -> 1). This will minimize the error in our
+;;; final results
