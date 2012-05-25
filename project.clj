fade3e883350dcb834ad946bbc4254e0b68cb9b6
Sidhant Godiwala <sidhantgodiwala@morrigan.local>
2012-05-25 08:31:54 +0530

    introduction

--- project.clj
+++ project.clj
@@ -1,3 +1,9 @@
 (defproject sicp "0.1.0"
-  :description "SICP in Clojure"
+  :description "#SICP in Clojure
+
+These are my solutions to the exercises
+from Structure and Interpretation of Computer Programs
+
+All feedback welcome, I hope you find this useful and
+have as much fun working through these as I did"
   :dependencies [[org.clojure/clojure "1.4.0"]])
