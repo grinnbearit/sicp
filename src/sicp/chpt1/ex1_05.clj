999794d3e905fa663f06a73a6dedaed937900193
Sidhant Godiwala <sidhantgodiwala@morrigan.(none)>
2012-05-26 19:51:57 +0530

    chpt1 ex1.5

--- src/sicp/chpt1/ex1_05.clj
+++ src/sicp/chpt1/ex1_05.clj
@@ -1,5 +1,8 @@
 (ns sicp.chpt1.ex1-05)
 
 
-;;; applicative order => infinite loop / stack overflow
-;;; normal order => 0
+;;; In applicative order, the arguments are evaluated before being passed
+;;; into the function. The argument `y` resolves to `(p)` which causes an infinite loop or a stack overflow
+
+;;; Normal order, resolves `x` since its used first and terminates, returning 0 without ever resolving
+;;; `y`
