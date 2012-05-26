5e8ee7de8c7f1fb09205747a31f96cb93ddc7dbf
Sidhant Godiwala <sidhantgodiwala@morrigan.(none)>
2012-05-26 19:52:27 +0530

    chpt1 ex1.6

--- src/sicp/chpt1/ex1_06.clj
+++ src/sicp/chpt1/ex1_06.clj
@@ -1,8 +1,8 @@
 (ns sicp.chpt1.ex1-06)
 
 
-;;; (sqrt-iter (improve guess x) x)
-;;; would be evaluated whether (good-enough? guess x) was true or not
-;;; this would result in an infinite call to sqrt-iter
+;;; `(sqrt-iter (improve guess x) x)`
+;;; is evaluated whether `(good-enough? guess x)` was true or not,
+;;; this would result in an infinite call to `sqrt-iter`
 
 ;;; This is only in the case of applicative order evaluation
