88a531d6e2b2a10466d071b0ce030d871e80029d
Sidhant Godiwala <sidhantgodiwala@morrigan.(none)>
2012-05-26 19:50:51 +0530

    chpt1 ex1.3

--- src/sicp/chpt1/ex1_03.clj
+++ src/sicp/chpt1/ex1_03.clj
@@ -11,20 +11,29 @@
   (+ (sqr x) (+ sqr y)))
 
 
+;;; Each conditional step adds more information until we can make a decision
+
+;;; 1. x < y,z
+;;; 2. z < x < y
+;;; 3. y < x < z
+;;; 4. y < z < x
+;;; 5. z < y < x
+
+
 (defn sum-of-greater-squares
   [x y z]
   (cond
-   (and (< x y) (< x z))                ; x < y,z
+   (and (< x y) (< x z))
    (sum-of-squares y z)
 
-   (< x y)                              ; z < x < y
+   (< x y)
    (sum-of-squares x y)
 
-   (< x z)                              ; y < x < z
+   (< x z)
    (sum-of-squares x z)
 
-   (< y z)                              ; y < z < x
+   (< y z)
    (sum-of-squares z x)
 
-   :else                                ; z < y < x
+   :else
    (sum-of-squares y x)))
