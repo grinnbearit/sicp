62e39c96e82dda88d8c3fe02ae945b1ebf45165e
Sidhant Godiwala <sidhantgodiwala@morrigan.(none)>
2012-05-26 19:50:21 +0530

    chpt1 ex1.2

--- src/sicp/chpt1/ex1_02.clj
+++ src/sicp/chpt1/ex1_02.clj
@@ -1,6 +1,14 @@
 (ns sicp.chpt1.ex1-02)
 
 
+;;; Using prefix notation has 2 advantages
+
+;;; First, your operators aren't treated differently from
+;;; other functions and so you have consistency
+
+;;; Second, precedence is obvious
+
+
 (/ (+ 5
       4
       (- 2
