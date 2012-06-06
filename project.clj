4bda5fd9bc2b153c72fc7bfd41c928a4df1d6e0c
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-06 10:21:28 +0530

    chpt1 ex1.14

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@ from Structure and Interpretation of Computer Programs
 
 All feedback welcome, I hope you find this useful and
 have as much fun working through these as I did"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.trace "0.7.3"]]
   :marginalia {:javascript ["mathjax/MathJax.js?config=default"]})
--- src/sicp/chpt1/ex1_14.clj
+++ src/sicp/chpt1/ex1_14.clj
@@ -1,69 +1,146 @@
-(ns sicp.chpt1.ex1-14)
+(ns sicp.chpt1.ex1-14
+  (:require [clojure.tools.trace :as t]))
 
 
-;;; Expansion of count-change
-;;; Indentation indicates recursive calls
+;;; Each call to `cc` results in 2 more calls to `cc`, also `amount` decrements by 1 when `kinds of coins` is 1.
+;;; Therefore, the upper bound time complexity is \\(O(2^{amount})\\).
+
+;;; Since the maximum recursive depth is when `kinds-of-coins` is 1. the space complexity is \\(O(amount)\\)
+
+(defn first-denomination
+  [kinds-of-coins]
+  (cond (= kinds-of-coins 1) 1
+        (= kinds-of-coins 2) 5
+        (= kinds-of-coins 3) 10
+        (= kinds-of-coins 4) 25
+        (= kinds-of-coins 5) 50))
 
 
-(count-change 11)
-(cc 11 5)
-  (cc 11 4)
-    (cc 11 3)
-      (cc 11 2)
-        (cc 11 1)
-          (cc 11 0)
-          (cc 10 1)
-            (cc 10 0)
-            (cc 9 1)
-              (cc 9 0)
-              (cc 8 1)
-                (cc 8 0)
-                (cc 7 1)
-                  (cc 7 0)
-                  (cc 6 1)
-                    (cc 6 0)
-                    (cc 5 1)
-                      (cc 5 0)
-                      (cc 4 1)
-                        (cc 4 0)
-                        (cc 3 1)
-                          (cc 3 0)
-                          (cc 2 1)
-                            (cc 2 0)
-                            (cc 1 1)
-                              (cc 1 0)
-                              (cc 0 1)
-        (cc 6 2)
-          (cc 6 1)
-            (cc 6 0)
-            (cc 5 1)
-              (cc 5 0)
-              (cc 4 1)
-                (cc 4 0)
-                (cc 3 1)
-                  (cc 3 0)
-                  (cc 2 1)
-                    (cc 2 0)
-                    (cc 1 1)
-                      (cc 1 0)
-                      (cc 0 1)
-          (cc 1 2)
-            (cc 1 1)
-              (cc 1 0)
-              (cc 0 1)
-            (cc -4 2)
-      (cc 1 3)
-        (cc 1 2)
-          (cc 1 1)
-            (cc 1 0)
-            (cc 0 1)
-          (cc -4 2)
-        (cc -9 3)
-    (cc -14 4)
-  (cc -39 5)
+(t/deftrace cc
+  [amount kinds-of-coins]
+  (cond (= amount 0) 1
+        (or (< amount 0) (= kinds-of-coins 0)) 0
+        :else
+        (+ (cc amount
+               (- kinds-of-coins 1))
+           (cc (- amount
+                  (first-denomination kinds-of-coins))
+               kinds-of-coins))))
 
+(defn count-change
+  [amount]
+  (cc amount 5))
 
-;;; Each call to cc results in 2 more calls to cc, also the amount drops by 1 when 'kinds of coins' is 1
-;;; Therefore I'm making the assumption (without rigorous proof) that the upper bound time complexity is O(2^Amt)
+;;; Using `clojure.tools.trace` to trace the call path of `cc`
 
-;;; The space complexity is O(Amt) since the maximum number of recursive calls is when 'kinds of coins' is 1
+;;     (count-change 11)
+;;     (cc 11 5)
+;;     | (cc 11 4)
+;;     | | (cc 11 3)
+;;     | | | (cc 11 2)
+;;     | | | | (cc 11 1)
+;;     | | | | | (cc 11 0)
+;;     | | | | | => 0
+;;     | | | | | (cc 10 1)
+;;     | | | | | | (cc 10 0)
+;;     | | | | | | => 0
+;;     | | | | | | (cc 9 1)
+;;     | | | | | | | (cc 9 0)
+;;     | | | | | | | => 0
+;;     | | | | | | | (cc 8 1)
+;;     | | | | | | | | (cc 8 0)
+;;     | | | | | | | | => 0
+;;     | | | | | | | | (cc 7 1)
+;;     | | | | | | | | | (cc 7 0)
+;;     | | | | | | | | | => 0
+;;     | | | | | | | | | (cc 6 1)
+;;     | | | | | | | | | | (cc 6 0)
+;;     | | | | | | | | | | => 0
+;;     | | | | | | | | | | (cc 5 1)
+;;     | | | | | | | | | | | (cc 5 0)
+;;     | | | | | | | | | | | => 0
+;;     | | | | | | | | | | | (cc 4 1)
+;;     | | | | | | | | | | | | (cc 4 0)
+;;     | | | | | | | | | | | | => 0
+;;     | | | | | | | | | | | | (cc 3 1)
+;;     | | | | | | | | | | | | | (cc 3 0)
+;;     | | | | | | | | | | | | | => 0
+;;     | | | | | | | | | | | | | (cc 2 1)
+;;     | | | | | | | | | | | | | | (cc 2 0)
+;;     | | | | | | | | | | | | | | => 0
+;;     | | | | | | | | | | | | | | (cc 1 1)
+;;     | | | | | | | | | | | | | | | (cc 1 0)
+;;     | | | | | | | | | | | | | | | => 0
+;;     | | | | | | | | | | | | | | | (cc 0 1)
+;;     | | | | | | | | | | | | | | | => 1
+;;     | | | | | | | | | | | | | | => 1
+;;     | | | | | | | | | | | | | => 1
+;;     | | | | | | | | | | | | => 1
+;;     | | | | | | | | | | | => 1
+;;     | | | | | | | | | | => 1
+;;     | | | | | | | | | => 1
+;;     | | | | | | | | => 1
+;;     | | | | | | | => 1
+;;     | | | | | | => 1
+;;     | | | | | => 1
+;;     | | | | => 1
+;;     | | | | (cc 6 2)
+;;     | | | | | (cc 6 1)
+;;     | | | | | | (cc 6 0)
+;;     | | | | | | => 0
+;;     | | | | | | (cc 5 1)
+;;     | | | | | | | (cc 5 0)
+;;     | | | | | | | => 0
+;;     | | | | | | | (cc 4 1)
+;;     | | | | | | | | (cc 4 0)
+;;     | | | | | | | | => 0
+;;     | | | | | | | | (cc 3 1)
+;;     | | | | | | | | | (cc 3 0)
+;;     | | | | | | | | | => 0
+;;     | | | | | | | | | (cc 2 1)
+;;     | | | | | | | | | | (cc 2 0)
+;;     | | | | | | | | | | => 0
+;;     | | | | | | | | | | (cc 1 1)
+;;     | | | | | | | | | | | (cc 1 0)
+;;     | | | | | | | | | | | => 0
+;;     | | | | | | | | | | | (cc 0 1)
+;;     | | | | | | | | | | | => 1
+;;     | | | | | | | | | | => 1
+;;     | | | | | | | | | => 1
+;;     | | | | | | | | => 1
+;;     | | | | | | | => 1
+;;     | | | | | | => 1
+;;     | | | | | => 1
+;;     | | | | | (cc 1 2)
+;;     | | | | | | (cc 1 1)
+;;     | | | | | | | (cc 1 0)
+;;     | | | | | | | => 0
+;;     | | | | | | | (cc 0 1)
+;;     | | | | | | | => 1
+;;     | | | | | | => 1
+;;     | | | | | | (cc -4 2)
+;;     | | | | | | => 0
+;;     | | | | | => 1
+;;     | | | | => 2
+;;     | | | => 3
+;;     | | | (cc 1 3)
+;;     | | | | (cc 1 2)
+;;     | | | | | (cc 1 1)
+;;     | | | | | | (cc 1 0)
+;;     | | | | | | => 0
+;;     | | | | | | (cc 0 1)
+;;     | | | | | | => 1
+;;     | | | | | => 1
+;;     | | | | | (cc -4 2)
+;;     | | | | | => 0
+;;     | | | | => 1
+;;     | | | | (cc -9 3)
+;;     | | | | => 0
+;;     | | | => 1
+;;     | | => 4
+;;     | | (cc -14 4)
+;;     | | => 0
+;;     | => 4
+;;     | (cc -39 5)
+;;     | => 0
+;;     => 4
