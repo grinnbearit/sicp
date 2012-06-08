353515f3c5be31801d7b6d62064f711cf0970192
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-08 18:58:18 +0530

    chpt1 ex1.20

--- src/sicp/chpt1/ex1_20.clj
+++ src/sicp/chpt1/ex1_20.clj
@@ -1,43 +1,73 @@
 (ns sicp.chpt1.ex1-20)
 
 
-;;; Normal Order
+;;; __Normal Order__
+
+;;; Evaluating `gcd` with normal order
 (gcd 206
-     40)                                ; 0, remainder count; each call to '=' evaluates b
+     40)
+
+;;; each call to `(= b 0)` evaluates `b`
+
+;;; initial number of calls to rem is 0
 
 (gcd 40
-     (rem 206 40))                      ; +1 
+     (rem 206 40))
+
+;;; +1 call to `rem`
 
 (gcd (rem 206 40)
-     (rem 40 (rem 206 40)))             ; +2
+     (rem 40 (rem 206 40)))
+
+;;; +2 calls to `rem`
 
 (gcd (rem 40 (rem 206 40))
-     (rem (rem 206 40) (rem 40 (rem 206 40)))) ; +4
+     (rem (rem 206 40) (rem 40 (rem 206 40))))
+
+;;; +4 calls to `rem`
 
 (gcd (rem (rem 206 40) (rem 40 (rem 206 40)))
-     (rem (rem 40 (rem 206 40)) (rem (rem 206 40) (rem 40 (rem 206 40))))) ; +7
+     (rem (rem 40 (rem 206 40)) (rem (rem 206 40) (rem 40 (rem 206 40)))))
 
-;;; Finally returning 'a' which is another +4
-;;; Number of calls to rem 0 + 1 + 2 + 4 + 7 + 4 -> 18
+;;; +7 calls to `rem`
+
+;;; Finally +4 calls to `rem` when returning `a`
+
+;;; Total number of calls to `rem` with normal order
+
+;;; \\(0 + 1 + 2 + 4 + 7 + 4 \rightarrow 18\\)
 
 
-;;; Applicative Order
+;;; __Applicative Order__
+
+;;; Evaluating `gcd` with applicative order
 (gcd 206
-     40)                                ; 0, remainder count; each call to 'rem' evaluates immediately
+     40)
+
+;;; Each call to `gcd` evaluates both arguments immediately
+
+;;; initial number of calls to rem is 0
 
 (gcd 40
-     6)                                 ; +1
+     6)
 
+;;; +1 call to `rem`
 
 (gcd 6
-     4)                                 ; +1
+     4)
+
+;;; +1 call to `rem`
 
 (gcd 4
-     2)                                 ; +1
+     2)
+
+;;; +1 call to `rem`
 
 (gcd 2
-     0)                                 ; +1
+     0)
 
+;;; Finally `a` is already evaluated so no additional calls to `rem` required
 
-;;; 'a' is already evaluated
-;;; Number of calls to rem 0 + 1 + 1 + 1 + 1 -> 4
+;;; Total number of calls to `rem` with applicative order
+
+;;; \\(0 + 1 + 1 + 1 + 1 \rightarrow 4\\)
