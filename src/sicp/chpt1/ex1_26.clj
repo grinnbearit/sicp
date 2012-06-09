62a7b4feba5879a72ae8315c6c970d0120c41c2b
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-09 17:29:58 +0530

    chpt1 ex1.26

--- src/sicp/chpt1/ex1_26.clj
+++ src/sicp/chpt1/ex1_26.clj
@@ -1,8 +1,8 @@
 (ns sicp.chpt1.ex1-26)
 
 
-;; The log(n) runtime from the original expmod algorithm works by halving the number of steps
-;; each time we find an even power
+;; The original `expmod` function has a runtime of \\(O(\log _2 n\\) because it halves the number of
+;; remaining steps each time we find an even `exp`
 
-;; by calling expmod twice when multiplying, Louis loses this advantage, calculating half the number of steps
-;; twice and then multiplying them instead of squaring the result once, thus making expmod O(n).
+;; by calling `expmod` twice, Louis loses this advantage, calculating half the number of steps
+;; twice, thus making \\(O(expmod) \rightarrow n\\)
