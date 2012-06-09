c9d0637aaab63e0adf8be685b6c57e0116e66458
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-09 17:30:06 +0530

    chpt1 ex1.27

--- src/sicp/chpt1/ex1_27.clj
+++ src/sicp/chpt1/ex1_27.clj
@@ -58,29 +58,68 @@
 
 ;; known primes and non primes
 ;;
-;; (fermat-prime? 1009) => true
-;; (prime? 1009) => true
-;; (fermat-prime? 1011) => false
-;; (prime? 1011) => false
-;; (fermat-prime? 10007) => true
-;; (prime? 10007) => true
-;; (fermat-prime? 10011) => false
-;; (prime? 10011) => false
+;;     (fermat-prime? 1009)
+;;     => true
+;;
+;;     (prime? 1009)
+;;     => true
+;;
+;;     (fermat-prime? 1011)
+;;     => false
+;;
+;;     (prime? 1011)
+;;     => false
+;;
+;;     (fermat-prime? 10007)
+;;     => true
+;;
+;;     (prime? 10007)
+;;     => true
+;;
+;;     (fermat-prime? 10011)
+;;     => false
+;;
+;;     (prime? 10011)
+;;     => false
+;;
 
 ;; Carmichael numbers
 ;;
-;; (fermat-prime? 561) => true
-;; (prime? 561) => false
-;; (fermat-prime? 1105) => true
-;; (prime? 1105) => false
-;; (fermat-prime? 1729) => true
-;; (prime? 1729) => false
-;; (fermat-prime? 2465) => true
-;; (prime? 2465) => false
-;; (fermat-prime? 2821) => true
-;; (prime? 2821) => false
-;; (fermat-prime? 6601) => true
-;; (prime? 6601) => false
+;;     (fermat-prime? 561)
+;;     => true
+;;
+;;     (prime? 561)
+;;     => false
+;;
+;;     (fermat-prime? 1105)
+;;     => true
+;;
+;;     (prime? 1105)
+;;     => false
+;;
+;;     (fermat-prime? 1729)
+;;     => true
+;;
+;;     (prime? 1729)
+;;     => false
+;;
+;;     (fermat-prime? 2465)
+;;     => true
+;;
+;;     (prime? 2465)
+;;     => false
+;;
+;;     (fermat-prime? 2821)
+;;     => true
+;;
+;;     (prime? 2821)
+;;     => false
+;;
+;;     (fermat-prime? 6601)
+;;     => true
+;;
+;;     (prime? 6601)
+;;     => false
 
 
 ;; All the Carmichael numbers in the footnote fool the fermat-prime test
