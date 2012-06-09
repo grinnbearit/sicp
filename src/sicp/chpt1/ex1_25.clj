4af1ce140da5664c4d815a3dd31e760bed2d98ee
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-09 17:29:50 +0530

    chpt1 ex1.25

--- src/sicp/chpt1/ex1_25.clj
+++ src/sicp/chpt1/ex1_25.clj
@@ -1,7 +1,7 @@
 (ns sicp.chpt1.ex1-25)
 
 
-;; Alyssa's code could work for smaller numbers but raising even 10 to 100 would overflow in most languages and would
+;; Alyssa's code would work for smaller numbers but  even \\(10^{100}\\) would overflow in most languages and would
 ;; need slower, arbitrary precision arithmetic in Clojure.
 
-;; The original expmod algorithm works with primitive arithmetic operations, even on very large numbers.
+;; The `expmod` function works with primitive arithmetic operations, even on very large numbers.
