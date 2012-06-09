b4f9f0d096d9cae19c624717a458d2b953b49b6d
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-09 17:29:44 +0530

    chpt1 ex1.24

--- src/sicp/chpt1/ex1_24.clj
+++ src/sicp/chpt1/ex1_24.clj
@@ -57,42 +57,42 @@
 
 ;; Each test was run 100,000 times before the time was recorded to account for HotSpot optimization
 
-;; (take 3 (find-timed-primes-between 1000 1000000000))
+;; `(take 3 (find-timed-primes-between 1000 1000000000))`
 
-;; 1009 0.043298 msec
-;; 1013 0.044814 msec
-;; 1019 0.042874 msec
+;; * 1009, 0.043298 msec
+;; * 1013, 0.044814 msec
+;; * 1019, 0.042874 msec
 
-;; average time taken 0.04366 msec
+;; average time taken, 0.04366 msec
 
-;; (take 3 (find-timed-primes-between 10000 1000000000))
+;; `(take 3 (find-timed-primes-between 10000 1000000000))`
 
-;; 10007 0.062862 msec
-;; 10009 0.064997 msec
-;; 10037 0.06907 msec
+;; * 10007, 0.062862 msec
+;; * 10009, 0.064997 msec
+;; * 10037, 0.06907 msec
 
-;; average time taken 0.065654 msec
+;; average time taken, 0.065654 msec
 
-;; (take 3 (find-timed-primes-between 100000 1000000000))
+;; `(take 3 (find-timed-primes-between 100000 1000000000))`
 
-;; 100003 0.084971 msec
-;; 100019 0.083323 msec
-;; 100043 0.083917 msec
+;; * 100003, 0.084971 msec
+;; * 100019, 0.083323 msec
+;; * 100043, 0.083917 msec
 
-;; average time taken 0.08407 msec
+;; average time taken, 0.08407 msec
 
-;; (take 3 (find-timed-primes-between 1000000 1000000000))
+;; `(take 3 (find-timed-primes-between 1000000 1000000000))`
 
-;; 1000003 0.116445 msec
-;; 1000033 0.119691 msec
-;; 1000037 0.108891 msec
+;; * 1000003, 0.116445 msec
+;; * 1000033, 0.119691 msec
+;; * 1000037, 0.108891 msec
 
-;; average time taken 0.11501 msec
+;; average time taken, 0.11501 msec
 
-;; Since the growth is log(n), I would expect that the time to test primes near 10^6 is
-;; roughly log(1000) times greater that testing primes near 10^3, roughly 10 times larger
+;; Since the growth is \\(\log _2 n\\), I would have expected the time to test primes near \\(10^6\\) was
+;; roughly \\(\log _2 1000\\) times greater that testing primes near \\(10^3\\), roughly 10 times larger
 
-;; The real result is closer to 2.5 times
+;; It was closer to 2.5 times larger
 
-;; This discrepancy is probably due to the Stochastic nature of this algorithm which can terminate in
-;; a fewer number of steps if the primality test fails earlier
+;; This discrepancy is probably due to the stochastic nature of this algorithm, which can terminate earlier
+;; if the primality test fails
