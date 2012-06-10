52b29538e975a0e9397bb7d82fd26244a170d507
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-10 19:28:26 +0530

    chpt1 ex1.34

--- src/sicp/chpt1/ex1_34.clj
+++ src/sicp/chpt1/ex1_34.clj
@@ -1,11 +1,15 @@
 (ns sicp.chpt1.ex1-34)
 
-;; We'd get an error since 2 is not a function
+;; Evaluating `(f f)`, we'd get an error since 2 is not a function
 
 (defn f
   [g]
   (g 2))
 
 
-;; (f f)
-;; => java.lang.Long cannot be cast to clojure.lang.IFn
+;;     (f f)
+;;     |
+;;     (f 2)
+;;     |
+;;     (2 2)
+;;     => java.lang.Long cannot be cast to clojure.lang.IFn
