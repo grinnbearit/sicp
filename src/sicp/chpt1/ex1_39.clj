abf80f3d4b91538fc6e632486156e2fa05f58cfe
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-11 18:16:13 +0530

    chpt1 ex1.39

--- src/sicp/chpt1/ex1_39.clj
+++ src/sicp/chpt1/ex1_39.clj
@@ -24,14 +24,22 @@
     (float (cont-frac numerator denominator k))))
 
 
-;; (tan-cf (* Math/PI 0) 11)
-;; => 0.0
+;;; \\(\tan 0 = 0\\)
 
-;; (tan-cf (* Math/PI 1/4) 11)
-;; => 1.0
+;;;     (tan-cf (* Math/PI 0) 11)
+;;;     => 0.0
 
-;; (tan-cf (* Math/PI 1/2) 11)
-;; => undefined
+;;; \\(\tan \frac{\pi}{4} = 1\\)
 
-;; (tan-cf (* Math/PI 3/4) 11)
-;; => -1.0
+;;;     (tan-cf (* Math/PI 1/4) 11)
+;;;     => 1.0
+
+;; \\(\tan \frac{\pi}{2} = \infty\\)
+
+;;;     (tan-cf (* Math/PI 1/2) 11)
+;;;     => *Divide By Zero*
+
+;; \\(\tan \frac{3\pi}{4} = -1\\)
+
+;;;     (tan-cf (* Math/PI 3/4) 11)
+;;;     => -1.0
