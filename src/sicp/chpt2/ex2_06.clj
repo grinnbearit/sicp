28fc5a27565a2824e8cc46797e787ea6ed457a41
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-16 12:40:21 +0530

    chpt2 ex2.06

--- src/sicp/chpt2/ex2_06.clj
+++ src/sicp/chpt2/ex2_06.clj
@@ -1,6 +1,9 @@
 (ns sicp.chpt2.ex2-06)
 
 
+;;; An integer \\(n\\) corresponds to n applications of a function `f` as a church numeral
+
+
 (defn church->int
   [n]
   ((n inc) 0))
@@ -31,20 +34,23 @@
       (f (f x)))))
 
 
+;;; Addition is simply function composition
+
+
 (defn add
   [a b]
   (fn [f]
     (comp (a f) (b f))))
 
 
-;;; (church->int zero)
-;;; => 0
+;;;     (church->int zero)
+;;;     => 0
 
-;;; (church->int one)
-;;; => 1
+;;;     (church->int one)
+;;;     => 1
 
-;;; (church->int two)
-;;; => 2
+;;;     (church->int two)
+;;;     => 2
 
-;;; (church->int (add one two))
-;;; => 3
+;;;     (church->int (add one two))
+;;;     => 3
