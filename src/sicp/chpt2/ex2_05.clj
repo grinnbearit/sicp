351dc9bf8af5905bdbc578ea6662bb756a753f06
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-16 12:40:15 +0530

    chpt2 ex2.05

--- src/sicp/chpt2/ex2_05.clj
+++ src/sicp/chpt2/ex2_05.clj
@@ -2,11 +2,16 @@
   (:refer-clojure :exclude [cons]))
 
 
+;;; Since both 2 and 3 are prime, \\(2^{a}3^{b}\\) is unique to the pair (a, b)
+
+
 (defn cons
   [x y]
   (int (* (Math/pow 2 x)
           (Math/pow 3 y))))
 
+
+;;; `d-component` computes the largest integer value of x which satisfies \\(d^x \leq n\\)
 
 (defn- d-component
   [n d]
@@ -26,9 +31,20 @@
   (d-component z 3))
 
 
-;;; (cons 2 3)
-;;; (car (cons 2 3))
-;;; => 2
+;;;     (car (cons 2 3))
+;;;     => 2
 
-;;; (cdr (cons 2 3))
-;;; => 3
+;;;     (cdr (cons 2 3))
+;;;     => 3
+
+
+;;; Using the substitution model
+
+;;;     (car (cons 2 3))
+;;;     # (car 108)
+;;;     => 2
+
+
+;;;     (cdr (cons 2 3))
+;;;     # (cdr 108)
+;;;     => 3
