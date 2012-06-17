5c229e82a96c3c78ced37392d83a5dc2b82c5d57
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-17 13:00:40 +0530

    chpt2 ex2.15

--- src/sicp/chpt2/ex2_15.clj
+++ src/sicp/chpt2/ex2_15.clj
@@ -1,15 +1,14 @@
 (ns sicp.chpt2.ex2-15)
 
 
-;;; The actual formula for calculating resitances in parallel
-;;; 1/[(1/R1) + (1/R2)]
+;;; The formula for calculating resitances in parallel is \\(\frac{1}{R1} + \frac{1}{R2}\\)
 
-;;; Simplifying the formula by one step we get
-;;; 1/[(R2 + R1)/R1R2]
+;;; Simplifying the formula by one step we get \\(\frac{1}{\frac{R2 + R1}{R1R2}}\\)
 
-;;; But this simplification makes the assumption that R1/R1 and R2/R2 are 1
-;;; but using interval arithmetic, they aren't exactly 1
+;;; This makes the assumption that \\(\frac{R1}{R1}\\) and \\(\frac{R2}{R2}\\) are 1
 
-;;; Therefore the simplification is wrong
+;;; But with interval arithmetic, they aren't exactly 1
 
-;;; par2 is the more accurate formula simply because it is the actual formula without simplification
+;;; Therefore the simplification is not possible
+
+;;; `par2` is the more accurate formula because it doesn't make this assumption
