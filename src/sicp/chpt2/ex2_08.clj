29d803b789020924be8f5b8c3d6d93e6e4719959
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2012-06-17 13:00:15 +0530

    chpt2 ex2.08

--- src/sicp/chpt2/ex2_08.clj
+++ src/sicp/chpt2/ex2_08.clj
@@ -28,3 +28,8 @@
     (if (< a b)
       (make-interval a b)
       (make-interval b a))))
+
+
+;;     (sub-interval (make-interval 4 5)
+;;                   (make-interval 2 3))
+;;     => #sicp.chpt2.ex2_08.Interval{:lb 2, :ub 2}
